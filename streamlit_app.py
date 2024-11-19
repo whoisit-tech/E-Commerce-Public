530f48bb643b9cdc4f5db1f248b1f28fcc816c53
whoisit-tech <ochadwnta@gmail.com>
2024-11-19 21:41:57 +0700

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+# Judul Aplikasi
+st.title("Analisis Data E-Commerce")
+st.subheader("Proyek Analisis Data")
+
+# Penjelasan Aplikasi
+st.write("""
+Selamat datang di aplikasi analisis data e-commerce.
+Aplikasi ini memberikan insight berdasarkan dataset yang digunakan dalam proyek ini.
+""")
