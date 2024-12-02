aceb95cabd0cbec874adc72b933f1722d630db12
whoisit-tech <ochadwnta@gmail.com>
2024-12-02 15:45:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # Data untuk grafik pertama (Top Produk Berdasarkan Jumlah Penjualan)
 data_penjualan = {
